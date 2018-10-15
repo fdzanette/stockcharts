7629a0dcd82ba39e462c191a7eb1407726291e0b
fabricio zanette <fabzanette@gmail.com>
2018-10-15 16:43:03 -0300

    add echart.js library. make candlestick charts work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,27 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker')
+
+//var echarts = require('echarts');
+
+const echarts = require('echarts');
+
+// initialize echarts instance with prepared DOM
+const myChart = echarts.init(document.getElementById('main'));
+// draw chart
+console.log(myChart);
+myChart.setOption({
+    xAxis: {
+        data: ['2017-10-24', '2017-10-25', '2017-10-26', '2017-10-27']
+    },
+    yAxis: {},
+    series: [{
+        type: 'k',
+        data: [
+            [20, 30, 10, 35],
+            [40, 35, 30, 55],
+            [33, 38, 33, 40],
+            [40, 40, 32, 42]
+        ]
+    }]
+});
