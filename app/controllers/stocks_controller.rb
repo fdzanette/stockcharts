fc2c5a338327f9cb6a8fe66ca33c6a9b0e7913c0
fabricio zanette <fabzanette@gmail.com>
2018-10-16 18:20:20 -0300

    set stock name when loading prices

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,12 +1,12 @@
 class StocksController < ApplicationController
   def index
     @stocks = Stock.all
-    @stock = Stock.new
+    @stock = Stock.new(name: TradeDate.set_stock_name)
     @stock.trade_dates = TradeDate.set_stock_price_history
   end
 
   def show
-    @stock = Stock.find(params:id)
+    #@stock = Stock.find(params:id)
   end
 
   def new
--- app/models/trade_date.rb
+++ app/models/trade_date.rb
@@ -3,12 +3,11 @@ class TradeDate < ApplicationRecord
 
   def self.set_stock_price_history
     workbook = SimpleXlsxReader.open '/home/fabricio/Documents/Parsing_cotações.xlsx'
-
     worksheets = workbook.sheets
     chart_data = []
     worksheets.each do |worksheet|
       num_rows = 0
-
+      Stock.new(name:worksheet.name)
       worksheet.rows.drop(1).each do |row|
         row_cells = row
          chart_data << TradeDate.new(day:row[0], open:row[1], high:row[1], low:row[1], close:row[1])
@@ -17,4 +16,15 @@ class TradeDate < ApplicationRecord
     end
   chart_data
   end
+
+  def self.set_stock_name
+    workbook = SimpleXlsxReader.open '/home/fabricio/Documents/Parsing_cotações.xlsx'
+    worksheets = workbook.sheets
+    stock_name = ""
+    worksheets.each do |worksheet|
+      stock_name = worksheet.name
+    end
+    stock_name
+  end
+
 end
