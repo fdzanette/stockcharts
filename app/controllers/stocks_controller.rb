7ebf85727994847fdfb9ca9093e5937c92a1df16
fabricio zanette <fabzanette@gmail.com>
2018-10-16 20:07:05 -0300

    read data from stock name only

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,12 +2,12 @@ class StocksController < ApplicationController
   def index
     @names = ["Petr4", "Vale5", "Bbas3"]
     @stocks = Stock.all
-    @stock = Stock.new(name: TradeDate.set_stock_name)
-    @stock.trade_dates = TradeDate.set_stock_price_history
+    #@stock = Stock.new(name: TradeDate.set_stock_name)
   end
 
   def show
     @stock = Stock.find(params[:id])
+    @stock.trade_dates = TradeDate.set_stock_price_history(@stock.name)
   end
 
   def new
--- app/models/trade_date.rb
+++ app/models/trade_date.rb
@@ -1,30 +1,31 @@
 class TradeDate < ApplicationRecord
   belongs_to :stock
 
-  def self.set_stock_price_history
+  def self.set_stock_price_history(stock_name)
     workbook = SimpleXlsxReader.open '/home/fabricio/Documents/Parsing_cotações.xlsx'
     worksheets = workbook.sheets
     chart_data = []
     worksheets.each do |worksheet|
       num_rows = 0
-      Stock.new(name:worksheet.name)
-      worksheet.rows.drop(1).each do |row|
-        row_cells = row
-         chart_data << TradeDate.new(day:row[0], open:row[1], high:row[1], low:row[1], close:row[1])
-        num_rows += 1
+      if stock_name == worksheet.name
+          worksheet.rows.drop(1).each do |row|
+            row_cells = row
+             chart_data << TradeDate.new(day:row[0], open:row[1], high:row[1], low:row[1], close:row[1])
+            num_rows += 1
+          end
       end
     end
   chart_data
   end
 
-  def self.set_stock_name
-    workbook = SimpleXlsxReader.open '/home/fabricio/Documents/Parsing_cotações.xlsx'
-    worksheets = workbook.sheets
-    stock_name = ""
-    worksheets.each do |worksheet|
-      stock_name = worksheet.name
-    end
-    stock_name
-  end
+  # def self.set_stock_name
+  #   workbook = SimpleXlsxReader.open '/home/fabricio/Documents/Parsing_cotações.xlsx'
+  #   worksheets = workbook.sheets
+  #   stock_name = ""
+  #   worksheets.each do |worksheet|
+  #     stock_name = worksheet.name
+  #   end
+  #   stock_name
+  # end
 
 end
