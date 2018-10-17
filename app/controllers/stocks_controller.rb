279bfd9789b02c2c28dcfd4052aed769bdff83f1
fabricio zanette <fabzanette@gmail.com>
2018-10-17 10:39:32 -0300

    chart drawing not workinh with echarts

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -6,9 +6,6 @@ class StocksController < ApplicationController
   def show
     @stock = Stock.find(params[:id])
     @stock.trade_dates = TradeDate.set_stock_price_history(@stock.name)
-    respond_to do |format|
-      format.js
-    end
   end
 
   def new
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,22 +1,21 @@
 // layout file, like app/views/layouts/application.html.erb
-//= require xlsx
 
-// const echarts = require('echarts');
-// // initialize echarts instance with prepared DOM
-// const myChart = echarts.init(document.getElementById('main'));
-// // draw chart
-// myChart.setOption({
-//     xAxis: {
-//         data: ['2017-10-24', '2017-10-25', '2017-10-26', '2017-10-27']
-//     },
-//     yAxis: {},
-//     series: [{
-//         type: 'k',
-//         data: [
-//             [20, 30, 10, 35],
-//             [40, 35, 30, 55],
-//             [33, 38, 33, 40],
-//             [40, 40, 32, 42]
-//         ]
-//     }]
-// });
+const echarts = require('echarts');
+// initialize echarts instance with prepared DOM
+const myChart = echarts.init(document.getElementById('main'));
+// draw chart
+myChart.setOption({
+    xAxis: {
+        data: ['2017-10-24', '2017-10-25', '2017-10-26', '2017-10-27']
+    },
+    yAxis: {},
+    series: [{
+        type: 'k',
+        data: [
+            [20, 30, 10, 35],
+            [40, 35, 30, 55],
+            [33, 38, 33, 40],
+            [40, 40, 32, 42]
+        ]
+    }]
+});
