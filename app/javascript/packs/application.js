e16be6a5947ab152c9d5f2b45d8101e41e215283
fabricio zanette <fabzanette@gmail.com>
2018-10-17 14:31:58 -0300

    fix table price problem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,21 +1,43 @@
 // layout file, like app/views/layouts/application.html.erb
 
-const echarts = require('echarts');
-// initialize echarts instance with prepared DOM
-const myChart = echarts.init(document.getElementById('main'));
-// draw chart
-myChart.setOption({
-    xAxis: {
-        data: ['2017-10-24', '2017-10-25', '2017-10-26', '2017-10-27']
-    },
-    yAxis: {},
-    series: [{
-        type: 'k',
-        data: [
-            [20, 30, 10, 35],
-            [40, 35, 30, 55],
-            [33, 38, 33, 40],
-            [40, 40, 32, 42]
-        ]
-    }]
-});
+// const echarts = require('echarts');
+// // initialize echarts instance with prepared DOM
+// const myChart = echarts.init(document.getElementById('main'));
+// // draw chart
+// myChart.setOption({
+//     xAxis: {
+//         data: ['2017-10-24', '2017-10-25', '2017-10-26', '2017-10-27']
+//     },
+//     yAxis: {},
+//     series: [{
+//         type: 'k',
+//         data: [
+//             [20, 30, 10, 35],
+//             [40, 35, 30, 55],
+//             [33, 38, 33, 40],
+//             [40, 40, 32, 42]
+//         ]
+//     }]
+// });
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+  function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['Mon', 20, 28, 38, 45],
+      ['Tue', 31, 38, 55, 66],
+      ['Wed', 50, 55, 77, 80],
+      ['Thu', 77, 77, 66, 50],
+      ['Fri', 68, 66, 22, 15]
+      // Treat first row as data as well.
+    ], true);
+
+    var options = {
+      legend:'none'
+    };
+
+    var chart = new google.visualization.CandlestickChart(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+  }
--- app/models/trade_date.rb
+++ app/models/trade_date.rb
@@ -10,7 +10,7 @@ class TradeDate < ApplicationRecord
       if stock_name == worksheet.name
           worksheet.rows.drop(1).each do |row|
             row_cells = row
-             chart_data << TradeDate.new(day:row[0], open:row[1], high:row[1], low:row[1], close:row[1])
+             chart_data << TradeDate.new(day:row[0], open:row[1], high:row[2], low:row[3], close:row[4])
             num_rows += 1
           end
       end
