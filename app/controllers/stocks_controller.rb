8f950177662c48ceb3804123dc03742f8d27efdc
fabricio zanette <fabzanette@gmail.com>
2018-10-19 15:53:22 -0300

    display moving average

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,11 +1,39 @@
 class StocksController < ApplicationController
+  before_action :set_stock, only: [:show, :set_ema]
+
   def index
     @stocks = Stock.all
   end
 
   def show
-    @stock = Stock.find(params[:id])
-    @stock.trade_dates = TradeDate.set_stock_price_history(@stock.name)
+    @moving_average1 = set_ema1
+    @moving_average2 = set_ema2
+    @moving_average3 = set_ema3
+
+  end
+
+  def set_ema1
+    average_array = []
+    @stock.trade_dates.each do |trade|
+      average_array << trade.close
+    end
+    average_array.ema(3,3)
+  end
+
+  def set_ema2
+    average_array = []
+    @stock.trade_dates.each do |trade|
+      average_array << trade.close
+    end
+    average_array.ema(6,6)
+  end
+
+  def set_ema3
+    average_array = []
+    @stock.trade_dates.each do |trade|
+      average_array << trade.close
+    end
+    average_array.ema(10,10)
   end
 
   def new
@@ -21,6 +49,11 @@ class StocksController < ApplicationController
 
   def stock_params
     params.require(:stock).permit(:name)
+  end
+
+  def set_stock
+    @stock = Stock.find(params[:id])
+    @stock.trade_dates = TradeDate.set_stock_price_history(@stock.name)
   end
 
 end
