96dbbadda616cc3c309e73988aff31f442d95b58
fabricio zanette <fabzanette@gmail.com>
2018-10-19 18:47:36 -0300

    colour close cells when compared to average

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,20 @@
+//import "bootstrap";
+
 const btnStrategy = document.getElementById('set');
+const stockClose = document.querySelectorAll('.stock_close');
+const stockExpoAverage = document.querySelectorAll('.expo_average');
+
+
+btnStrategy.addEventListener('click', (event) => {
+  let i = 0;
+  stockClose.forEach((close) => {
+    if (close.innerText < stockExpoAverage[i].innerText) {
+      close.classList.toggle('sell');
+    } else if (close.innerText > stockExpoAverage[i].innerText){
+      close.classList.toggle('buy');
+    };
+    console.log(stockExpoAverage[i].innerText);
+    //console.log(stockExpoAverage[i].innerHTML);
+    i += 1
+  });
+});
