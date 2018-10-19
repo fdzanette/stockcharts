a22eb982065529e7bbfc2dc2c9d11e22ca67e75a
fabricio zanette <fabzanette@gmail.com>
2018-10-19 15:59:15 -0300

    refactor average method

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,39 +1,14 @@
 class StocksController < ApplicationController
-  before_action :set_stock, only: [:show, :set_ema]
+  before_action :set_stock, only: [:show, :set_moving_average]
 
   def index
     @stocks = Stock.all
   end
 
   def show
-    @moving_average1 = set_ema1
-    @moving_average2 = set_ema2
-    @moving_average3 = set_ema3
-
-  end
-
-  def set_ema1
-    average_array = []
-    @stock.trade_dates.each do |trade|
-      average_array << trade.close
-    end
-    average_array.ema(3,3)
-  end
-
-  def set_ema2
-    average_array = []
-    @stock.trade_dates.each do |trade|
-      average_array << trade.close
-    end
-    average_array.ema(6,6)
-  end
-
-  def set_ema3
-    average_array = []
-    @stock.trade_dates.each do |trade|
-      average_array << trade.close
-    end
-    average_array.ema(10,10)
+    @moving_average1 = set_moving_average(3,3)
+    @moving_average2 = set_moving_average(6,6)
+    @moving_average3 = set_moving_average(10,6)
   end
 
   def new
@@ -54,6 +29,14 @@ class StocksController < ApplicationController
   def set_stock
     @stock = Stock.find(params[:id])
     @stock.trade_dates = TradeDate.set_stock_price_history(@stock.name)
+  end
+
+  def set_moving_average(x,y)
+    average_array = []
+    @stock.trade_dates.each do |trade|
+      average_array << trade.close
+    end
+    average_array.ema(x,y)
   end
 
 end
