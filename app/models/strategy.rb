9a4083bcf8973fdbd4edb0e02c8af163d3a20a37
fabricio zanette <fabzanette@gmail.com>
2018-10-22 17:59:22 -0300

    def 3 strategy

--- app/models/strategy.rb
+++ app/models/strategy.rb
@@ -11,31 +11,21 @@ class Strategy < ApplicationRecord
             count += 1
             buy = true
             paid = date.close
-            puts "Media: #{expo_average.ema(row - 1, 5).round(2)}"
-            puts "Comprou -- Preço: #{paid} -- Data: #{date.day}"
-            puts "----------"
           end
         elsif date.close < expo_average.ema(row -1, 5)
           if buy == true
             buy = false
             sold = date.close
             profit += (sold - paid) * amount
-            puts "Media: #{expo_average.ema(row - 1, 5).round(2)}"
-            puts "Vendeu -- Preço: #{sold} -- Data: #{date.day}"
-            puts "Lucro na operação: #{sold - paid}"
-            puts "Lucro acumulado: #{profit}"
-            puts "------------------------------"
           end
         end
       end
     expo_average << date.close
     row += 1
     end
-    puts "Total de operações: #{count}"
-    puts "---END---"
   end
 
-def set_strategy2(stock, amount) #cruzamento de mme de 5 periodos com o preço de fechamento
+  def set_strategy2(stock, amount) #cruzamento de mme de 5 periodos com o preço de fechamento
     expo_average = []
     row = count = paid = sold = profit = 0
     buy = false
@@ -46,11 +36,36 @@ def set_strategy2(stock, amount) #cruzamento de mme de 5 periodos com o preço d
             count += 1
             buy = true
             paid = date.close
+          end
+        elsif expo_average.ema(row - 1, 5) < expo_average.ema(row - 1, 13)
+          if buy == true
+            buy = false
+            sold = date.close
+            profit += (sold - paid) * amount
+          end
+        end
+      end
+    expo_average << date.close
+    row += 1
+    end
+  end
+
+  def set_strategy2(stock, amount) #cruzamento de mme de 5 periodos com o preço de fechamento
+    expo_average = []
+    row = count = paid = sold = profit = 0
+    buy = false
+    stock.trade_dates.reverse.each do |date|
+      if row > 21
+        if expo_average.ema(row - 1, 5) > expo_average.ema(row - 1, 21)
+          if buy == false
+            count += 1
+            buy = true
+            paid = date.close
             puts "Media: #{expo_average.ema(row - 1, 5).round(2)}"
             puts "Comprou -- Preço: #{paid} -- Data: #{date.day}"
             puts "----------"
           end
-        elsif expo_average.ema(row - 1, 5) < expo_average.ema(row - 1, 13)
+        elsif expo_average.ema(row - 1, 5) < expo_average.ema(row - 1, 21)
           if buy == true
             buy = false
             sold = date.close
