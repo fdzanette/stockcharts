8757163480bc4df00fcce1519fed19e70acd9cdf
fabricio zanette <fabzanette@gmail.com>
2018-10-23 15:24:40 -0300

    def 4th strategy

--- app/models/strategy.rb
+++ app/models/strategy.rb
@@ -57,6 +57,29 @@ class Strategy < ApplicationRecord
     stock.trade_dates.each do |date|
       if row >= 21
         if expo_average.ema(row - 1, 5) > expo_average.ema(row - 1, 21) && buy == false
+            count += 1
+            buy = true
+            paid = date.close
+        elsif expo_average.ema(row - 1, 5) < expo_average.ema(row - 1, 8)
+          if buy == true
+            buy = false
+            sold = date.close
+            profit += (sold - paid) * amount
+          end
+        end
+      end
+    expo_average << date.close
+    row += 1
+    end
+  end
+
+  def set_strategy4(stock, amount) #cruzamento de mme de 5 periodos com 21 na compra. venda no cruzamento do close com mme 5.
+    expo_average = []
+    row = count = paid = sold = profit = 0
+    buy = false
+    stock.trade_dates.each do |date|
+      if row >= 21
+        if expo_average.ema(row - 1, 5) > expo_average.ema(row - 1, 21) && buy == false
           #if buy == false
             count += 1
             buy = true
@@ -65,7 +88,7 @@ class Strategy < ApplicationRecord
             puts "Comprou -- Preço: #{paid} -- Data: #{date.day}"
             puts "----------"
           #end
-        elsif expo_average.ema(row - 1, 5) < expo_average.ema(row - 1, 8)
+        elsif date.close < expo_average.ema(row -1, 5)
           if buy == true
             buy = false
             sold = date.close
@@ -84,6 +107,5 @@ class Strategy < ApplicationRecord
     puts "Total de operações: #{count}"
     puts "---END---"
   end
-
 
 end
