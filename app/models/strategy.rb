bb47ccb878ce9e46c19014b10dac7b75f02b601d
fabricio zanette <fabzanette@gmail.com>
2018-10-25 15:51:56 -0300

    display result of strategies

--- app/models/strategy.rb
+++ app/models/strategy.rb
@@ -1,14 +1,14 @@
 class Strategy < ApplicationRecord
 
   def set_strategy1(stock, amount) #cruzamento de mme de 5 periodos com o preço de fechamento
-    expo_average = []
-    row = count = paid = sold = @profit = 0
+    @data = expo_average = []
+    row = @count = paid = sold = @profit = 0
     buy = false
     stock.trade_dates.each do |date|
       if row >= 5
         if date.close > expo_average.ema(row - 1, 5)
           if buy == false
-            count += 1
+            @count += 1
             buy = true
             paid = date.close
           end
@@ -23,18 +23,18 @@ class Strategy < ApplicationRecord
     expo_average << date.close
     row += 1
     end
-    @profit
+    @data = [@profit, @count]
   end
 
   def set_strategy2(stock, amount) #cruzamento de mme de 5 periodos com a media de 13
-    expo_average = []
-    row = count = paid = sold = @profit = 0
+    @date = expo_average = []
+    row = @count = paid = sold = @profit = 0
     buy = false
     stock.trade_dates.each do |date|
       if row >= 13
         if expo_average.ema(row - 1, 5) > expo_average.ema(row - 1, 13)
           if buy == false
-            count += 1
+            @count += 1
             buy = true
             paid = date.close
           end
@@ -49,17 +49,17 @@ class Strategy < ApplicationRecord
     expo_average << date.close
     row += 1
     end
-    @profit
+    @data = [@profit, @count]
   end
 
   def set_strategy3(stock, amount) #cruzamento de mme de 5 periodos com 21 na compra. venda no cruzamento de 5 e 8.
-    expo_average = []
-    row = count = paid = sold = @profit = 0
+    @data = expo_average = []
+    row = @count = paid = sold = @profit = 0
     buy = false
     stock.trade_dates.each do |date|
       if row >= 21
         if expo_average.ema(row - 1, 5) > expo_average.ema(row - 1, 21) && buy == false
-            count += 1
+            @count += 1
             buy = true
             paid = date.close
         elsif expo_average.ema(row - 1, 5) < expo_average.ema(row - 1, 8)
@@ -73,17 +73,17 @@ class Strategy < ApplicationRecord
     expo_average << date.close
     row += 1
     end
-    @profit
+    @data = [@profit, @count]
   end
 
   def set_strategy4(stock, amount) #cruzamento de mme de 5 periodos com 21 na compra. venda no cruzamento do close com mme 5.
-    expo_average = []
-    row = count = paid = sold = @profit = 0
+    @data = expo_average = []
+    row = @count = paid = sold = @profit = 0
     buy = false
     stock.trade_dates.each do |date|
       if row >= 21
         if expo_average.ema(row - 1, 5) > expo_average.ema(row - 1, 21) && buy == false
-            count += 1
+            @count += 1
             buy = true
             paid = date.close
         elsif date.close < expo_average.ema(row -1, 5)
@@ -97,16 +97,16 @@ class Strategy < ApplicationRecord
     expo_average << date.close
     row += 1
     end
-    @profit
+    @data = [@profit, @count]
   end
 
   def set_strategy5(stock, amount) #compra a qualquer preço. venda a 6% de lucro ou stop a 2,5%
-    expo_average = []
-    count = paid = sold = @profit = 0
+    @data = expo_average = []
+    @count = paid = sold = @profit = 0
     buy = false
     stock.trade_dates.each do |date|
         if buy == false
-            count += 1
+            @count += 1
             buy = true
             paid = date.close
             puts "Comprou -- Preço: #{paid} -- Data: #{date.day}"
@@ -120,9 +120,9 @@ class Strategy < ApplicationRecord
         end
     expo_average << date.close
     end
-    puts "Total de operações: #{count}"
+    puts "Total de operações: #{@count}"
     puts "Resultado: R$ #{@profit}"
     puts "---END---"
-    @profit
+    @data = [@profit, @count]
   end
 end
